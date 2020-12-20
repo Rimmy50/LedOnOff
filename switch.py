3d9ebd93b6044a38846d107dad979bae985ea4ee
Rimmy50 <nsaroiu@hotmail.com>
2020-12-20 10:18:07 -0800

    Update switch.py

--- switch.py
+++ switch.py
@@ -7,12 +7,12 @@ ser = serial.Serial('COM4', 9600)
 param = int(sys.argv[1])
 
 
-# Turn LED on is 1 is passed as the parameter
+# To turn LED on, 1 is passed as the parameter
 if param == 1:
     # Delay is added to ensure the arduino receives the function
     for i in range(100):
         ser.write('O'.encode())
         time.sleep(0.000001)
-# Turn LED off is 0 is passed as the parameter
+# To turn LED off, 0 is passed as the parameter
 elif param == 0:
     ser.write('F'.encode())
